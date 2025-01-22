1a442f190d7a641adbc3a2d0de40ea73cbbc992c
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-01-22 11:35:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 function sum(a) {
+    if (a.length == 0) return 0;
     var sum = a[0];
-    for(var i = 0; i < a.length; i++) {
+    for(var i = 1; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
